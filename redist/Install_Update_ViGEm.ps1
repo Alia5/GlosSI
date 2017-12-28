4489b9d3d7f407bd961b59480d5e905f9918b77c
Peter Repukat <orly.alia5@gmail.com>
2017-12-28 22:34:02 +0100

    ViGEmScript: Force Admin privi.

--- redist/Install_Update_ViGEm.ps1
+++ redist/Install_Update_ViGEm.ps1
@@ -1,3 +1,14 @@
+
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{   
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Break
+Return
+}
+
+
+
 Register-PSRepository -Name nuget.vigem.org -SourceLocation https://nuget.vigem.org/ -InstallationPolicy Trusted -confirm:$false
 
 Install-Module ViGEmManagementModule -Repository nuget.vigem.org
