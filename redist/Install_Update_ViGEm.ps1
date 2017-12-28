9ee1bed7a73c7d84632e8bbc23284ed342839550
Peter Repukat <orly.alia5@gmail.com>
2017-12-28 22:37:46 +0100

    ViGemScript: add vigem device removal, just in case

--- redist/Install_Update_ViGEm.ps1
+++ redist/Install_Update_ViGEm.ps1
@@ -8,13 +8,16 @@ Return
 }
 
 
+#Remove multiple just in case... 
+Get-ViGEmBusDevice | Remove-ViGEmBusDevice
+Get-ViGEmBusDevice | Remove-ViGEmBusDevice
+Get-ViGEmBusDevice | Remove-ViGEmBusDevice
+Get-ViGEmBusDevice | Remove-ViGEmBusDevice
+
 
 Register-PSRepository -Name nuget.vigem.org -SourceLocation https://nuget.vigem.org/ -InstallationPolicy Trusted -confirm:$false
 
 Install-Module ViGEmManagementModule -Repository nuget.vigem.org
-
-#Remove just in case... 
-Get-ViGEmBusDevice | Remove-ViGEmBusDevice
 
 Install-ViGEmBusDeviceDriver
 
