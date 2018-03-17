41741c16300f5d25ba78e050fdc01a0cd7ee3748
Peter Repukat <orly.alia5@gmail.com>
2018-03-17 21:41:24 +0100

    fix ViGEm install script

--- redist/Install_Update_ViGEm.ps1
+++ redist/Install_Update_ViGEm.ps1
@@ -7,17 +7,12 @@ Break
 Return
 }
 
-if (get-module | Where-Object {$_.Name -eq "ViGEmManagementModule"}) 
+if (!(Get-Module -Listavailable -Name "ViGEmManagementModule"))
 {
-    $res =  Get-ViGEmBusDevice 
-    if ($res -ne $null)
-    {
-        Get-ViGEmBusDevice | Remove-ViGEmBusDevice
-    }
-} else {
-    Register-PSRepository -Name nuget.vigem.org -SourceLocation https://nuget.vigem.org/ -InstallationPolicy Trusted -force
+    Register-PSRepository -Name nuget.vigem.org -SourceLocation https://nuget.vigem.org/ -InstallationPolicy Trusted
+    Install-Module ViGEmManagementModule -Repository nuget.vigem.org
 }
 
-Install-Module ViGEmManagementModule -Repository nuget.vigem.org
-Install-ViGEmBusDeviceDriver
+Get-ViGEmBusDevice | Remove-ViGEmBusDevice
 Add-ViGEmBusDevice
+Install-ViGEmBusDeviceDriver
