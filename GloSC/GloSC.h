cce035b6e989eb84e4f004efc4be11119a97c798
Peter Repukat <orly.alia5@gmail.com>
2018-03-18 14:48:37 +0100

    GloSC-Config: Clang tidy fixes

--- GloSC/GloSC.h
+++ GloSC/GloSC.h
@@ -43,10 +43,10 @@ class GloSC : public QMainWindow
 	Q_OBJECT
 
 public:
-	GloSC(QWidget *parent = Q_NULLPTR);
+	explicit GloSC(QWidget *parent = Q_NULLPTR);
 
 private:
-	Ui::GloSCClass ui;
+	Ui::GloSCClass ui_;
 
 	void updateEntryList();
 	void writeIni(QString entryName) const;
@@ -55,17 +55,17 @@ private:
 
 	void check360ControllerRebinding();
 
-	QList<UWPPair> uwpPairs;
+	QList<UWPPair> uwp_pairs_;
 
-	const unsigned int GLOSC_VERSION = 0x00000512; //Version Number in as bytes, just remove the dots.
+	constexpr static const unsigned int GLOSC_VERSION = 0x00000512; //Version Number in as bytes, just remove the dots.
 
-	int wide_x = 711;
-	int small_x = 302;
+	int wide_x_ = 711;
+	int small_x_ = 302;
 
-	int wide_x_create = 261;
-	int small_x_create = 131;
+	int wide_x_create_ = 261;
+	int small_x_create_ = 131;
 
-	QGraphicsOpacityEffect opEff;
+	QGraphicsOpacityEffect op_eff_;
 
 	void animate(int to);
 
