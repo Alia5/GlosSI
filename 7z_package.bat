672ef31024f0b68183e035b6d0bbafa93d9a8705
Peter Repukat <orly.alia5@gmail.com>
2018-03-18 17:20:34 +0100

    update packagescript / installer

---
+++ 7z_package.bat
@@ -0,0 +1,9 @@
+SET QTDIR="C:\Qt\5.10.1\msvc2015"
+SET APPVEYOR_BUILD_FOLDER="D:\Alia5\Documents\Visual Studio Projects\GloSC"
+
+"C:\Program Files\7-Zip\7z.exe" a GloSC.zip TargetConfig.ini License.txt qt-license.txt Readme.md Build_Install.md %APPVEYOR_BUILD_FOLDER%\build\Win32\Release\*.exe %APPVEYOR_BUILD_FOLDER%\build\Win32\Release\*.dll %APPVEYOR_BUILD_FOLDER%\dependencies\SFML-2.4.2-x86\bin\sfml-system-2.dll %APPVEYOR_BUILD_FOLDER%\dependencies\SFML-2.4.2-x86\bin\sfml-window-2.dll %APPVEYOR_BUILD_FOLDER%\dependencies\SFML-2.4.2-x86\bin\sfml-graphics-2.dll redist\* %QTDIR%\bin\Qt5Core.dll %QTDIR%\bin\Qt5Gui.dll %QTDIR%\bin\Qt5Widgets.dll %QTDIR%\plugins\platforms\qwindows.dll
+
+"C:\Program Files\7-Zip\7z.exe" rn GloSC.zip qwindows.dll platforms\qwindows.dll
+
+"C:\Program Files\7-Zip\7z.exe" rn GloSC.zip redist/libeay32.dll libeay32.dll
+"C:\Program Files\7-Zip\7z.exe" rn GloSC.zip redist/ssleay32.dll ssleay32.dll
--- 7z_package_x64.bat
+++
@@ -1,6 +0,0 @@
-SET QTDIR="C:\Qt\5.9\msvc2017_64"
-SET APPVEYOR_BUILD_FOLDER="D:\Alia5\Documents\Visual Studio Projects\GloSC"
-
-"C:\Program Files\7-Zip\7z.exe" a GloSC_x64.zip TargetConfig.ini License.txt qt-license.txt Readme.md Build_Install.md %APPVEYOR_BUILD_FOLDER%\build\x64\Release\*.exe %APPVEYOR_BUILD_FOLDER%\build\Win32Only\Release\*.exe %APPVEYOR_BUILD_FOLDER%\build\Win32Only\Release\*.dll %APPVEYOR_BUILD_FOLDER%\dependencies\SFML-2.4.2-x64\bin\sfml-system-2.dll %APPVEYOR_BUILD_FOLDER%\dependencies\SFML-2.4.2-x64\bin\sfml-window-2.dll %APPVEYOR_BUILD_FOLDER%\dependencies\SFML-2.4.2-x64\bin\sfml-graphics-2.dll redist\* %QTDIR%\bin\Qt5Core.dll %QTDIR%\bin\Qt5Gui.dll %QTDIR%\bin\Qt5Widgets.dll %QTDIR%\plugins\platforms\qwindows.dll
-
-"C:\Program Files\7-Zip\7z.exe" rn GloSC_x64.zip qwindows.dll platforms\qwindows.dll
