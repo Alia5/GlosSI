6ab10cf03fd49a2a46e9079e6ef6d5124d76b4a9
Peter Repukat <orly.alia5@gmail.com>
2018-03-28 00:19:59 +0200

    Bump version / fix watchdog build

--- SteamTarget/VirtualControllerThread.cpp
+++ SteamTarget/VirtualControllerThread.cpp
@@ -15,6 +15,7 @@ limitations under the License.
 */
 #include "VirtualControllerThread.h"
 
+
 #include "../common/loguru.hpp"
 
 
--- common/Injector.cpp
+++ common/Injector.cpp
@@ -19,7 +19,6 @@ limitations under the License.
 
 #include "../common/loguru.hpp"
 
-
 void Injector::TakeDebugPrivilege()
 {
 	HANDLE hProcess = GetCurrentProcess(), hToken;
