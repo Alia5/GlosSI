5c2d6068c5d9c7196fb29d00cdd11d9b4bc3e0f5
Peter Repukat <orly.alia5@gmail.com>
2019-10-19 20:25:18 +0200

    Update Copyright

--- GloSC/GloSC.h
+++ GloSC/GloSC.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 Peter Repukat - FlatspotSoftware
+Copyright 2018-2019 Peter Repukat - FlatspotSoftware
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
