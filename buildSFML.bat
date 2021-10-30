90a8850c7fec7d3ffb6cebc4f4a61f1c2a0b87ec
Peter Repukat <orly.alia5@gmail.com>
2021-10-30 14:54:31 +0200

    Don't build debug sfml on CI

--- buildSFML.bat
+++ buildSFML.bat
@@ -1,7 +1,9 @@
 cd deps/SFML
 
+if  "%APPVEYOR%"=="" (
 cmake.exe -S . -B out/Debug -DCMAKE_BUILD_TYPE=Debug
 cmake.exe --build out/Debug --config Debug
+)
 
 cmake.exe -S . -B out/Release -DCMAKE_BUILD_TYPE=RelWithDebInfo
 cmake.exe --build out/Release --config RelWithDebInfo
