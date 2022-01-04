6b7127e1bb9713185d5d08c48e0bb1f9c96f4fc4
Peter Repukat <orly.alia5@gmail.com>
2022-01-04 23:37:33 +0100

    Change how UWP Apps are enumerated
    Now using powershell-script

---
+++ GlosSIConfig/GetAUMIDs.ps1
@@ -0,0 +1,40 @@
+#stolen and adapted from: https://github.com/BrianLima/UWPHook/blob/master/UWPHook/Resources/GetAUMIDScript.ps1
+$installedapps = get-AppxPackage
+$invalidNames = '*ms-resource*', '*DisplayName*'
+$aumidList = @()
+
+foreach ($app in $installedapps)
+{
+    try {
+            if(-not $app.IsFramework){
+            foreach ($id in (Get-AppxPackageManifest $app).package.applications.application.id)
+            {
+                    $appx = Get-AppxPackageManifest $app;
+                    $name = $appx.Package.Properties.DisplayName;
+
+                    if($name -like '*DisplayName*' -or $name  -like '*ms-resource*')
+                    {
+                        $name = $appx.Package.Applications.Application.VisualElements.DisplayName;
+                    }
+                    if($name -like '*DisplayName*' -or $name  -like '*ms-resource*')
+                    {
+                        $name = "-Error-";
+                    }
+
+                    $installDir = $app.InstallLocation;
+                    $logo = $app.InstallLocation + "\" + $appx.Package.Applications.Application.VisualElements.Square150x150Logo;
+
+
+                    $aumidList += $name + "|" + $installDir + "|" + $logo + "|" + 
+                    $app.packagefamilyname + "!" + $id + ";"
+                }
+            }
+        }
+        catch
+        {
+            $ErrorMessage = $_.Exception.Message
+            $FailedItem = $_.Exception.ItemName
+        }
+}
+
+$aumidList;
--- GlosSIConfig/main.cpp
+++ GlosSIConfig/main.cpp
@@ -110,6 +110,7 @@ int main(int argc, char* argv[])
 {
 #if defined(Q_OS_WIN)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    HRESULT hr = ::CoInitializeEx(NULL, COINIT_MULTITHREADED);
 #endif
 
     if (argc < 3) {
