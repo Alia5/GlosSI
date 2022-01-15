4fcd4a3bac8342aedbe96c26d7cb3fe99a8005b2
Peter Repukat <orly.alia5@gmail.com>
2022-01-15 19:17:07 +0100

    Fix vigembuildscript

--- buildViGEmClient.bat
+++ buildViGEmClient.bat
@@ -2,7 +2,7 @@ cd deps/ViGEmClient
 
 git apply ../../ViGEm_BuildConfig.patch
 
-msbuild.exe ViGEmCLient.sln /t:Build /p:Configuration=Debug_LIB;Platform=x64
-msbuild.exe ViGEmCLient.sln /t:Build /p:Configuration=Release_LIB;Platform=x64
+msbuild.exe ViGEmCLient.sln /t:Build /p:Configuration=Debug_LIB /p:Platform=x64
+msbuild.exe ViGEmCLient.sln /t:Build /p:Configuration=Release_LIB /p:Platform=x64
 
 cd ../..
