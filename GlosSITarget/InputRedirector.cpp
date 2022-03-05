654f201195a1e78f8ebaf1726a34dfdf834e59e8
Peter Repukat <orly.alia5@gmail.com>
2022-03-05 15:44:08 +0100

    auto open advanced controller settings

--- GlosSITarget/InputRedirector.cpp
+++ GlosSITarget/InputRedirector.cpp
@@ -67,7 +67,7 @@ void InputRedirector::run()
         bool enable_rumbe_copy = enable_rumble_;
         ImGui::Checkbox("Enable Rumble", &enable_rumbe_copy);
         enable_rumble_ = enable_rumbe_copy;
-        if (ImGui::CollapsingHeader("Advanced")) {
+        if (ImGui::CollapsingHeader("Advanced", ImGuiTreeNodeFlags_DefaultOpen)) {
             ImGui::Text("GlosSI uses different USB-IDs for it's emulated controllers");
             ImGui::Text("This can cause issues with some games");
             ImGui::Text("When changing this, it's advised to also set Max. controller count");
@@ -142,7 +142,7 @@ void InputRedirector::runLoop()
                     // Multiple controllers can be worked around with by setting max count.
                     if (!use_real_vid_pid_) {
                         vigem_target_set_vid(vt_x360_[i], 0x28de); //VALVE_DIRECTINPUT_GAMEPAD_VID
-                        // vigem_target_set_pid(vt_x360_[i], 0x11FF); //VALVE_DIRECTINPUT_GAMEPAD_PID   
+                        // vigem_target_set_pid(vt_x360_[i], 0x11FF); //VALVE_DIRECTINPUT_GAMEPAD_PID
                     }
                     // TODO: MAYBE!: In a future version, use something like OpenXInput
                     //and filter out emulated controllers to support a greater amount of controllers simultaneously
