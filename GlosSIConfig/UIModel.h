1ca127519a786a3d06f4070769f826d26874c580
Peter Repukat <orly.alia5@gmail.com>
2022-09-09 16:09:02 +0200

    Use shrotcuts_parser from submodule

--- GlosSIConfig/UIModel.h
+++ GlosSIConfig/UIModel.h
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #pragma once
-#include "ShortcutsVDF.h"
+#include <shortcuts_vdf.hpp>
 #include <QJsonObject>
 #include <QObject>
 #include <QVariant>
