9c12fd6febeb54ebebbccdde1d46c0206f662b20
Peter Repukat <orly.alia5@gmail.com>
2022-09-20 20:25:12 +0200

    Change/Fix use real vid/pid feature

--- GlosSITarget/InputRedirector.cpp
+++ GlosSITarget/InputRedirector.cpp
@@ -163,7 +163,10 @@ void InputRedirector::runLoop()
                     // Multiple controllers can be worked around with by setting max count.
                     if (!use_real_vid_pid_) {
                         vigem_target_set_vid(vt_pad_[i], 0x28de); //VALVE_DIRECTINPUT_GAMEPAD_VID
-                        // vigem_target_set_pid(vt_pad_[i], 0x11FF); //VALVE_DIRECTINPUT_GAMEPAD_PID
+                        vigem_target_set_pid(vt_pad_[i], 0x11FF); //VALVE_DIRECTINPUT_GAMEPAD_PID
+                    } else {
+                        vigem_target_set_vid(vt_pad_[i], 0x045E); // MICROSOFT
+                        vigem_target_set_pid(vt_pad_[i], 0x028E); // XBOX 360 Controller
                     }
                     // TODO: MAYBE!: In a future version, use something like OpenXInput
                     //and filter out emulated controllers to support a greater amount of controllers simultaneously
